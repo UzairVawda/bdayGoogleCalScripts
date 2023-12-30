acd2b5e3be9726e7bb92d6367a14f15d9c010f24
Uzair Vawda <uzairvawda@uzairs-mbp.mynetworksettings.com>
2023-12-30 11:14:10 -0500

    swithcing to command line promts rather than args

--- addSingularEvent.py
+++ addSingularEvent.py
@@ -1,16 +1,11 @@
 # ADD SINGULAR EVENT 
-# REQUIRED PARAMS
-# ADD -> currently add only
-# DURATION -> how long the event is scheduled for  
-# TITLE -> event title 
-
-
+# NO REQUIRED PARAMS
+# THE USER IS ASKED A SERIES OF QUESTIONS TO GENERATE ONE EVENT
 
 from __future__ import print_function
 
 import datetime
 import os.path
-from sys import argv
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -23,7 +18,6 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 # ADD YOUR CALENDAR ID HERE
-YOUR_CALENDAR_ID = 'uzair.vawda@gmail.com'
 YOUR_TIMEZONE = 'GMT-5' # find yours here: https://www.timezoneconverter.com/cgi-bin/zonehelp.tzc?cc=US&ccdesc=United%20States
 
 
@@ -43,26 +37,46 @@ def main():
 			with open('token.json', 'w') as token:
 					token.write(creds.to_json())
 
-	# determine which function to run        
-	if argv[1] == 'add':
-		duration = argv[2]
-		description = argv[3]
-		addEvent(creds, duration, description)
-	else:
-		print("THIS IS AN ADD ONLY FUNCTION")
-		return
+
+	# determine which function to run   
+	try:      
+		service = build('calendar', 'v3', credentials=creds)
+		calendarList = service.calendarList().list().execute();
+		# calendarList = calendarList.items
+		if 'items' in calendarList:
+			for index, calendar in enumerate(calendarList['items']):
+				print(f"{index + 1}) {calendar['summary']}")
+				# print(f"Calendar ID: {calendar['id']}")
+				# print("----------")
+	except HttpError as error:
+		print("Failed Calender Build and Event Fetch")
+		print(error)
+
+	calenderId = int(input("Select Calender: ")) - 1
+	print(calenderId)
+	print(calendarList)
+	print(calendarList['items'])
+	print(calendarList['items'][calenderId])
+	print(calendarList['items'][calenderId]['id'])
+	calenderId = calendarList['items'][calenderId]['id']
+	print(calenderId)
+	title = str(input("Event Title: "))
+	duration = int(input("Duration (in minutes): "))
+	description = str(input("Event Description: "))
+	addEvent(service, calenderId, title, duration, description)
+
 
     
-# add calendar event from curret time for length of 'duration'
-def addEvent(creds, duration, description):
+# add calendar event from current time for length of 'duration'
+def addEvent(service, calenderId, title, duration, description):
 	start = datetime.datetime.utcnow()
-	
-	end = datetime.datetime.utcnow() + datetime.timedelta(hours=int(duration))
+	end = datetime.datetime.utcnow() + datetime.timedelta(minutes=int(duration))
 	start_formatted = start.isoformat() + 'Z'
 	end_formatted = end.isoformat() + 'Z'
 
 	event = {
-	'summary': description,
+	'summary': title,
+	'description': description,
 	'start': {
 			'dateTime': start_formatted,
 			'timeZone': YOUR_TIMEZONE,
@@ -72,14 +86,7 @@ def addEvent(creds, duration, description):
 			'timeZone': YOUR_TIMEZONE,
 			},
 	}
-	print(event)
-
-	service = build('calendar', 'v3', credentials=creds)
-	print(service)
-
-	event = service.events().insert(calendarId=YOUR_CALENDAR_ID, body=event).execute()
-	print(event)
-
+	event = service.events().insert(calendarId=calenderId, body=event).execute()
 	print('Event created: %s' % (event.get('htmlLink')))
 
 
